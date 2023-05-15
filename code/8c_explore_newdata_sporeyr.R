46e71fba0b7aa833c40d24d8419178a9955923f8
wury5 <ruoyuwu@umich.edu>
2023-05-15 17:41:14 -0400

    analyze updated dataset

--- code/8c_explore_newdata_sporeyr.R
+++ code/8c_explore_newdata_sporeyr.R
@@ -1,7 +1,5 @@
 df_smooth_sporeyr <- read_rds(str_c(.path$dat_process, "2023-04-25/fill_smooth_sporeyr.rds"))
 df_metrics_sporeyr <- read_rds(str_c(.path$dat_process, "2023-04-25/metrics_sporeyr.rds"))
-df_smooth_sporeyr <- read_rds("/nfs/turbo/seas-zhukai/phenology/phenology_spore/processed/2023-04-25/fill_smooth_sporeyr.rds")
-df_metrics_sporeyr <- read_rds("/nfs/turbo/seas-zhukai/phenology/phenology_spore/processed/2023-04-25/metrics_sporeyr.rds")
 # seasonal integral
 ## select data
 data_integral <- df_smooth_sporeyr %>%
@@ -14,7 +12,7 @@ data_integral <- df_smooth_sporeyr %>%
   filter(observ_percent >= 0.5)
 
 # peak concentration and date
-ggplot(data = df_smooth_sporeyr %>% filter(n == 44)) +
+ggplot(data = df_smooth_sporeyr %>% filter(n == 105)) +
   geom_line(aes(x = sporeyr_date, y = count_smooth)) +
   scale_y_continuous(
     trans = scales::log_trans(),
@@ -23,10 +21,7 @@ ggplot(data = df_smooth_sporeyr %>% filter(n == 44)) +
   ) +
   scale_x_datetime(breaks = "1 year") +
   ylab("count") +
-  geom_vline(xintercept = (df_metrics_sporeyr %>% filter(n == 44) %>% mutate(peak_date = as.POSIXct(as.Date(peak_doy, origin = paste0(sporeyr, "-01-01"))) -1))$peak_date, color = "gray")
-44  46  48  49  51  52  54  55  56  57  60  61  64  66  67  68  69  71  74  78  79  81
-82  83  84  85  86  88  89  92  93  95  96 105
-
+  geom_vline(xintercept = (df_metrics_sporeyr %>% filter(n == 105) %>% mutate(peak_date = as.POSIXct(as.Date(peak_doy, origin = paste0(sporeyr, "-01-01"))) -1))$peak_date, color = "gray")
 ## select data
 data_peak <- df_metrics_sporeyr %>%
   filter(
@@ -44,35 +39,25 @@ data_peak <- df_metrics_sporeyr %>%
       (n == 36 & sporeyr != 2006 & sporeyr != 2010) |
       (n == 42 & sporeyr != 2002 & sporeyr != 2016 & sporeyr != 2017 & sporeyr != 2018 & sporeyr != 2019 & sporeyr != 2020) |
       (n == 44 & sporeyr != 2014 & sporeyr != 2022) |
-      
-    )%>%
-  dplyr::select(lat, lon, location, id, sporeyr, peak, peak_doy)
-data_peak <- df_metrics_sporeyr %>%
-  filter(
-    (location == "Austin Area, TX" & sporeyr %in% c(2013, 2014, 2015, 2016, 2017, 2018, 2020)) |
-      (location == "Bellevue, NE" & sporeyr %in% c(2012, 2013, 2014, 2018, 2019, 2020)) |
-      (location == "Dallas, TX" & sporeyr != 2008 & sporeyr != 2018) |
-      (location == "Dayton, OH" & sporeyr != 2008) |
-      (location == "Flower Mound, TX" & sporeyr != 2008) |
-      (location == "Houston 2, TX" & sporeyr %in% c(2011, 2012, 2013, 2014, 2015, 2018, 2019, 2020)) |
-      (location == "Kansas City, MO" & sporeyr != 2021) |
-      (location == "Mount Laurel, NJ" & sporeyr != 2014) |
-      (location == "Oklahoma City, OK" & sporeyr %in% c(2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2020)) |
-      (location == "Olean, NY" & sporeyr %in% c(2011, 2015, 2016, 2017, 2018, 2019, 2020, 2021)) |
-      (location == "Philadelphia, PA") |
-      (location == "San Antonio 3, TX" & sporeyr != 2013) |
-      (location == "San Antonio 2, TX" & sporeyr != 2011 & sporeyr != 2021) |
-      (location == "Silver Spring, MD" & sporeyr != 2008 & sporeyr != 2021) |
-      (location == "St. Louis, MO" & sporeyr != 2008 & sporeyr != 2021) |
-      (location == "Tampa, FL" & sporeyr %in% c(2009, 2010, 2011)) |
-      (location == "Tulsa, OK" & sporeyr %in% c(2009, 2012, 2013, 2014, 2016)) |
-      (location == "Twin Falls, ID" & sporeyr %in% c(2010, 2011, 2013, 2015, 2017, 2018)) |
-      (location == "Waterbury, CT" & sporeyr != 2011 & sporeyr != 2019 & sporeyr != 2021)) %>%
-  dplyr::select(lat, lon, location, id, sporeyr, peak, peak_doy)
-
-
-
-
+      (n == 46 & sporeyr != 2018) |
+      (n == 52 & sporeyr != 2018) |
+      (n == 55) |
+      (n == 57 & sporeyr %in% c(2003, 2004, 2005)) |
+      (n == 60) |
+      (n == 61 & sporeyr != 2021) |
+      (n == 64 & sporeyr %in% c(2006, 2007, 2008, 2010, 2013, 2016)) |
+      (n == 66) |
+      (n == 67 & sporeyr != 2004 & sporeyr != 2008) |
+      (n == 68 & sporeyr != 2022) |
+      (n == 69 & sporeyr != 2003 & sporeyr != 2004) |
+      (n == 71 & sporeyr %in% c(2008:2018)) |
+      (n == 78 & sporeyr %in% c(2007, 2008, 2011, 2012, 2013, 2014, 2018, 2019, 2020, 2022)) |
+      (n == 82 & sporeyr != 2003 & sporeyr != 2007 & sporeyr != 2008 & sporeyr != 2017 & sporeyr != 2018 & sporeyr != 2019) |
+      (n == 83 & sporeyr %in% c(2011, 2015, 2016, 2018, 2019, 2020, 2021, 2022)) |
+      (n == 89 & sporeyr != 2003 & sporeyr != 2004 & sporeyr != 2007 & sporeyr != 2008) |
+      (n == 95 & sporeyr != 2002 & sporeyr != 2007 & sporeyr != 2009) |
+      (n == 105 & sporeyr %in% c(2003, 2004, 2005, 2006, 2007, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020))
+    )
 
 # spore season
 ggplot(data = df_smooth_sporeyr %>% filter(n == 105)) +
@@ -86,7 +71,6 @@ ggplot(data = df_smooth_sporeyr %>% filter(n == 105)) +
   ylab("count") +
   geom_vline(xintercept = (df_metrics_sporeyr %>% filter(n == 105) %>% mutate(sos_date = as.POSIXct(as.Date(sos, origin = paste0(sporeyr, "-01-01"))) -1))$sos_date, color = "red") +
   geom_vline(xintercept = (df_metrics_sporeyr %>% filter(n == 105) %>% mutate(eos_date = as.POSIXct(as.Date(eos, origin = paste0(sporeyr, "-01-01"))) -1))$eos_date, color = "blue")
-
 ## select data
 data_season <- df_metrics_sporeyr %>%
   drop_na(sos) %>% 
