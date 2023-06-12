1f1771d58c0a30bf4a2b586761c1d7488942cb63
wury5 <ruoyuwu@umich.edu>
2023-06-12 00:16:30 -0400

    explore allergy season

--- code/7e_extract_metrics_by_data_completeness.R
+++ code/7e_extract_metrics_by_data_completeness.R
@@ -47,17 +47,69 @@ df_season <- df %>%
   ) %>%
   mutate(los = eos - sos)
 
-df_allergy_season <- df %>% 
-  filter(count_whit >= 6500) %>% 
+find_sas <- function(x) {
+  # find the first day of 10 consecutive days with spore count >= 6500
+  # and ensure 10 values before sas are not NA
+  # reverse order for eas
+  consecutive_count <- 0
+  you <- NA
+  you_date_old <- NA
+  for (i in 1:nrow(x)) {
+    if (!is.na(x$count_whit[i]) && x$count_whit[i] >= 6500) {
+      if (i > 10 && all(!is.na(x$count_whit[(i - 10):i]))) {
+        consecutive_count <- consecutive_count + 1
+        if (consecutive_count == 1) {
+          you <- x$doy_new[i]
+          you_date_old <- x$date[i]
+        }
+        if (consecutive_count == 10) {
+          break
+        }
+      } else if (i <= 10 && all(!is.na(x$count_whit[1:i]))) {
+        consecutive_count <- consecutive_count + 1
+        if (consecutive_count == 1) {
+          you <- x$doy_new[i]
+          you_date_old <- x$date[i]
+        }
+        if (consecutive_count == 10) {
+          break
+        }
+      } else {
+        consecutive_count <- 0
+      }
+    } else {
+      consecutive_count <- 0
+    }
+  }
+  x$you <- you
+  x$you_date_old <- you_date_old
+  return(x)
+}
+
+view(df_allergy_season %>% distinct(id, year_new, .keep_all = T) %>% drop_na(eas))
+
+df_allergy_season <- df_smooth %>% 
+  drop_na(count_whit) %>%
+  group_by(lat, lon, station, city, state, country, id, n, offset, year_new) %>%
+  summarise(observ_pct = n() / 365) %>%
+  right_join(df_smooth, by = c("lat", "lon", "station", "city", "state", "country", "id", "n", "offset", "year_new")) %>%
   group_by(lat, lon, station, city, state, country, id, n, offset, year_new, observ_pct) %>% 
-  summarise(
-    sas = min(doy_new),
-    eas = max(doy_new),
-    sas_date_old = min(date),
-    eas_date_old = max(date)
+  group_modify(~ find_sas(.)) %>% 
+  ungroup() %>% 
+  rename(
+    sas = you,
+    sas_date_old = you_date_old
+    ) %>% 
+  group_by(lat, lon, station, city, state, country, id, n, offset, year_new, observ_pct) %>% 
+  arrange(lat, lon, station, city, state, country, id, n, offset, year_new, observ_pct, desc(doy_new)) %>% 
+  group_modify(~ find_sas(.)) %>% 
+  arrange(lat, lon, station, city, state, country, id, n, offset, year_new, observ_pct, doy_new) %>% 
+  ungroup() %>% 
+  rename(
+    eas = you,
+    eas_date_old = you_date_old
   ) %>% 
-  
-  
+  mutate(las = )
 
 df_metrics <- list(df_peak, df_integral, df_season) %>% reduce(full_join, by = c("lat", "lon", "station", "city", "state", "country", "id", "n", "offset", "year_new", "observ_pct"))
 write_rds(df_metrics, str_c(.path$dat_process, "2023-04-25/metrics_offset_flags.rds"))
