347088fea6b5a83190e4c51d38c3640ef3e69a0a
wury5 <ruoyuwu@umich.edu>
2023-06-15 22:12:24 -0400

    explore metrics~climate

---
+++ code/17_results_lme_climate.R
@@ -0,0 +1,65 @@
+df_metrics <- read_rds(str_c(.path$dat_process, "2023-04-25/metrics_daymet.rds"))
+
+
+## peak
+data_peak <- df_metrics %>% 
+  filter(peak_check == 1) %>%
+  drop_na(peak) %>% 
+  filter(observ_pct >= 0.7) %>% 
+  group_by(lat, lon, station, city, state, country, id, n, offset) %>% 
+  mutate(start_year = min(year_new)) %>% 
+  mutate(end_year = max(year_new)) %>% 
+  mutate(Nyear = end_year - start_year + 1) %>%
+  mutate(Nrcd = n()) %>% 
+  filter(Nrcd >= 5) %>% 
+  filter(state != "PR") %>% 
+  filter(country == "US") %>% 
+  ungroup()
+# view(data_peak %>% distinct(n, .keep_all = T) %>% dplyr::select(lat, lon, city, state, n))
+
+# fit lme
+lme_climate_peak <- lme(
+  # log(peak) ~ mat + tap,
+  peak_doy ~ mat + tap,
+  data = data_peak,
+  random = ~ 1 | n
+  # ,
+  # control = lmeControl(opt = "optim")
+)
+summary(lme_climate_peak)
+
+
+
+
+
+## integral
+data_integral <- df_metrics %>% 
+  drop_na(integral) %>% 
+  filter(observ_pct >= 1) %>% 
+  group_by(lat, lon, station, city, state, country, id, n, offset) %>% 
+  mutate(start_year = min(year_new)) %>% 
+  mutate(end_year = max(year_new)) %>% 
+  mutate(Nyear = end_year - start_year + 1) %>%
+  mutate(Nrcd = n()) %>% 
+  filter(Nrcd >= 5) %>% 
+  filter(state != "PR") %>% 
+  filter(country != "CA") %>% 
+  ungroup()
+# view(data_integral %>% distinct(n, .keep_all = T) %>% dplyr::select(lat, lon, city, state, n))
+
+# fit lme
+lme_climate_integral <- lme(
+  integral ~ mat * tap,
+  # log(integral) ~ mat * tap,
+  data = data_integral,
+  random = ~ 1 | n
+  # ,
+  # control = lmeControl(opt = "optim")
+)
+summary(lme_climate_integral)
+
+
+
+
+
+## los
