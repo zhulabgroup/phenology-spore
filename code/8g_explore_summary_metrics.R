dd754c3e62954c3b2a823b56e5b8b392f0aef2be
wury5 <ruoyuwu@umich.edu>
2023-07-10 10:33:13 -0400

    fix labels on y axis

--- code/8g_explore_summary_metrics.R
+++ code/8g_explore_summary_metrics.R
@@ -16,10 +16,15 @@ df_summary <- df_metrics %>%
   mutate(Metric = ifelse(Metric == "peak_doy", "peak date", Metric))
 df_summary$Metric <- factor(df_summary$Metric, levels = c("peak", "peak date", "integral", "sos", "eos", "los", "sas", "eas", "las", "as integral"))
 
+labels_e <- function(x) {parse(text = gsub("e^", x))}
 p_metrics_1 <- ggplot(data = df_summary %>% filter(Metric %in% c("peak", "integral", "as integral")), aes(x = Metric, y = Value)) +
   geom_boxplot(width = 0.5) +
   theme_classic() +
-  ylab("ln(concentration)") +
+  ylab("Spore concentration\n(grains / m^3)") +
+  scale_y_continuous(
+    breaks = seq(5, 17, by = 2),
+    labels = scales::math_format(e^.x)
+    ) +
   theme(axis.line = element_line(colour = "black"),
         axis.text = element_text(size = 12, colour = "black"),
         axis.title = element_text(size = 14, colour = "black"))
