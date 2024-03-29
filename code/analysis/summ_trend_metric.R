da5aacd3704fd186a9ed6351ddd860ed943123ad
wury5 <ruoyuwu@umich.edu>
2024-03-29 00:36:00 -0400

    replot figs

--- code/analysis/summ_trend_metric.R
+++ code/analysis/summ_trend_metric.R
@@ -1,15 +1,20 @@
 source("~/Github/spore_phenology/code/analysis/calc_lme.R")
+df_median <- df_ana %>% 
+  group_by(Metric) %>% 
+  summarise(
+    med = median(Value),
+    n_station = length(unique(id))) %>% 
+  filter(Metric %in% c("SOS", "SAS", "EOS", "EAS", "LOS", "LAS", "ln_Ca", "ln_Cp", "ln_AIn", "ln_ASIn"))
 
 df_m <- data.frame()
 for (m_metric in c("SOS", "SAS", "EOS", "EAS", "LOS", "LAS", "ln_Ca", "ln_Cp", "ln_AIn", "ln_ASIn")) {
-  for (cplt in c(1, 0.9, 0.8, 0.7, 0.6)) {
-    m_rslt <- calc_lme(df_in = df_ana_full, metric = m_metric, x_vrb = "year_new", pct = cplt)
-    df_m <-rbind(df_m, m_rslt)
-  }
+  m_rslt <- calc_lme(df_in = df_ana, metric = m_metric, x_vrb = "year_new", pct = 0.8)
+  df_m <-rbind(df_m, m_rslt)
 }
-colnames(df_m) <- c("metric", "cpltness", "n_obsv", "change", "x_variable", "beta", "ci1", "ci2", "p")
+colnames(df_m) <- c("Metric", "cpltness", "n_obsv", "change", "x_variable", "beta", "ci1", "ci2", "p")
 
-df_sens_cpltness <- df_m %>% 
+df_summ_metric <- df_m %>% 
+  full_join(df_median, by = "Metric") %>% 
   mutate(cpltness = as.numeric(cpltness)) %>% 
   mutate(n_obsv = as.numeric(n_obsv)) %>% 
   mutate(change = as.numeric(change)) %>% 
@@ -17,17 +22,17 @@ df_sens_cpltness <- df_m %>%
   mutate(ci1 = as.numeric(ci1)) %>% 
   mutate(ci2 = as.numeric(ci2)) %>% 
   mutate(p = as.numeric(p)) %>% 
-  mutate(metric = case_when(
-    metric == "ln_Ca" ~ "ln(Ca)",
-    metric == "ln_Cp" ~ "ln(Cp)",
-    metric == "ln_AIn" ~ "ln(AIn)",
-    metric == "ln_ASIn" ~ "ln(ASIn)",
-    T ~ metric)) %>% 
+  mutate(Metric = case_when(
+    Metric == "ln_Ca" ~ "ln(Ca)",
+    Metric == "ln_Cp" ~ "ln(Cp)",
+    Metric == "ln_AIn" ~ "ln(AIn)",
+    Metric == "ln_ASIn" ~ "ln(ASIn)",
+    T ~ Metric)) %>% 
   mutate(x_variable = ifelse(x_variable == "year_new", "year", x_variable)) %>% 
   mutate(cpltness = paste0(cpltness * 100, "%")) %>% 
   mutate(n_obsv = n_obsv %>% as.character()) %>% 
   mutate(change_latent = ifelse(
-    metric %in% c("SOS", "SAS", "EOS", "EAS", "LOS", "LAS"),
+    Metric %in% c("SOS", "SAS", "EOS", "EAS", "LOS", "LAS"),
     paste0(change %>% round(2), "d"),
     paste0((change * 100) %>% round(2), "%"))) %>% 
   mutate(change = ifelse(
@@ -37,11 +42,11 @@ df_sens_cpltness <- df_m %>%
   mutate(beta = beta %>% round(4) %>% as.character()) %>% 
   mutate(p = p %>% round(4) %>% as.character()) %>% 
   rename(
-    "Metric" = "metric",
+    "Number of stations" = "n_station",
     "Data completeness" = "cpltness",
     "Number of observations" = "n_obsv",
     "Slope" = "beta",
     "Change" = "change",
     "p-value" = "p"
-    ) %>% 
-  dplyr::select("Metric", "Data completeness", "Number of observations", "Slope", "Change", "p-value")
+  ) %>% 
+  dplyr::select("Metric", "Number of stations", "Number of observations", "Slope", "Change", "p-value")
