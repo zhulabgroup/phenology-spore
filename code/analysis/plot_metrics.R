3e942262d3eb3de57921e3b4a8f82ea64ce03d9c
wury5 <ruoyuwu@umich.edu>
2024-04-02 22:24:17 -0400

    create series annotation figs

--- code/analysis/plot_metrics.R
+++ code/analysis/plot_metrics.R
@@ -15,3 +15,37 @@ p_metrics <- plot_grid(p_antt, p_metrics_con, p_metrics_pheno, p_metrics_in,
 #   plot_annotation(
 #     tag_levels = 'A',
 #     theme = theme(plot.title = element_text(size = 12)))
+
+
+
+
+
+source("~/Github/spore_phenology/code/analysis/plot_antt0.R")
+p_metrics0 <- plot_grid(p_antt0, p_metrics_con, p_metrics_pheno, p_metrics_in,
+                       ncol = 2, labels = c("A", "B", "C", "D"), label_fontface = "plain", label_size = 12,
+                       rel_heights = c(1, 1), rel_widths = c(2, 1))
+
+source("~/Github/spore_phenology/code/analysis/plot_antt1.R")
+p_metrics1 <- plot_grid(p_antt1, p_metrics_con, p_metrics_pheno, p_metrics_in,
+                        ncol = 2, labels = c("A", "B", "C", "D"), label_fontface = "plain", label_size = 12,
+                        rel_heights = c(1, 1), rel_widths = c(2, 1))
+
+source("~/Github/spore_phenology/code/analysis/plot_antt2.R")
+p_metrics2 <- plot_grid(p_antt2, p_metrics_con, p_metrics_pheno, p_metrics_in,
+                        ncol = 2, labels = c("A", "B", "C", "D"), label_fontface = "plain", label_size = 12,
+                        rel_heights = c(1, 1), rel_widths = c(2, 1))
+
+source("~/Github/spore_phenology/code/analysis/plot_antt3.R")
+p_metrics3 <- plot_grid(p_antt3, p_metrics_con, p_metrics_pheno, p_metrics_in,
+                        ncol = 2, labels = c("A", "B", "C", "D"), label_fontface = "plain", label_size = 12,
+                        rel_heights = c(1, 1), rel_widths = c(2, 1))
+
+source("~/Github/spore_phenology/code/analysis/plot_antt4.R")
+p_metrics4 <- plot_grid(p_antt4, p_metrics_con, p_metrics_pheno, p_metrics_in,
+                        ncol = 2, labels = c("A", "B", "C", "D"), label_fontface = "plain", label_size = 12,
+                        rel_heights = c(1, 1), rel_widths = c(2, 1))
+
+source("~/Github/spore_phenology/code/analysis/plot_antt5.R")
+p_metrics5 <- plot_grid(p_antt5, p_metrics_con, p_metrics_pheno, p_metrics_in,
+                        ncol = 2, labels = c("A", "B", "C", "D"), label_fontface = "plain", label_size = 12,
+                        rel_heights = c(1, 1), rel_widths = c(2, 1))
